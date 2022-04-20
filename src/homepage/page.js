ce9ca712d993d07a6b58a2b98b5542879fe291f9
Shane Park <shanejpark@gmail.com>
2022-04-19 20:37:35 -0400

    added main page

--- src/homepage/page.js
+++ src/homepage/page.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React from "react";
+import Header2 from "./header/header2.js";
+import Header3 from "./header/header3.js";
 
 class Homepage extends React.Component {
-    state={};
+  state = {};
 
-    componentDidMount() {
-        
-    }
+  componentDidMount() {}
 
-    render() {
-        return (
-            <>
-                Here is our new site!
-            </>
-        )
-    }
-};
+  render() {
+    return (
+      <>
+        <Header3></Header3>
+        <Header2></Header2>
+      </>
+    );
+  }
+}
 
 export default Homepage;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,5 +11,5 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
