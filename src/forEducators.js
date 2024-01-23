0bdea155d19a8a4a2b2214b4a2e46f52e5f3d2d4
MeghanaNandaKumar <nandakumar.me@northeastern.edu>
2024-01-22 20:44:19 -0500

    removed comments

--- src/forEducators.js
+++ src/forEducators.js
@@ -1,22 +1,6 @@
 import React from "react";
 import Stats from "../src/homepage/header/stats";
-// import StatsSection from "./header/stats.js";
-// import Header from "./header/header.js";
-// import FeaturedCurricula from "./featured/FeaturedCurricula.js";
-// import Navbar from "../navbar/navbar.js";
-// import EmailSection from "./contact/contactsection"                  //remove unwanted imports
 
-// class forEducators extends React.Component {
-//   state = {};
-
-//   componentDidMount() { }
-
-//   render() {
-//     return (
-//       <div>Hello World!</div>
-//     );
-//   }
-// }
 
 function ForEducators() { //function name can start with an uppercase or be camel case
   return (
