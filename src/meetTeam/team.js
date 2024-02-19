4799322bec979cdeeeccf4ccc0dd088ad8507f6e
ihong4 <92641118+ihong4@users.noreply.github.com>
2024-02-19 18:51:22 -0500

    more changes

--- src/meetTeam/team.js
+++ src/meetTeam/team.js
@@ -54,7 +54,7 @@ function header() {
         <Row style={{maxWidth: '100%', margin: '0px auto'}}>
         <div class="image-text-wrapper">
           {imageRow3(andy)}
-         <b>Why FirstByte?</b> The mission to promote diversity and equity in CS was something that I resonated, and with having being involved in many similar activities in high school, it seemed like a good fit for me when I was looking for clubs to join.
+        <p> <b>Why FirstByte?</b></p><h>The mission to promote diversity and equity in CS was something that I resonated, and with having being involved in many similar activities in high school, it seemed like a good fit for me when I was looking for clubs to join.</h>
           </div>
           {textRow3(
             <div>
@@ -64,7 +64,7 @@ function header() {
             )}
           <div class="image-text-wrapper">
           {imageRow3(caleb)}
-          <b>Why FirstByte?</b> I first joined FirstByte because I really wanted to be part of a club that I had interest in, not an academic extracurricular to pad my resume. I've always had a passion for teaching, and computer science was my major so it seemed natural. What I stumbled upon was a tight-knit family, where everyone is super passionate about what they do and supports each other to the fullest. I loved FirstByte's mission of making high-quality education accessible to anyone, and that's something I really wanted to be a part of. 
+          <p> <b>Why FirstByte?</b></p><h> I first joined FirstByte because I really wanted to be part of a club that I had interest in, not an academic extracurricular to pad my resume. I've always had a passion for teaching, and computer science was my major so it seemed natural. What I stumbled upon was a tight-knit family, where everyone is super passionate about what they do and supports each other to the fullest. I loved FirstByte's mission of making high-quality education accessible to anyone, and that's something I really wanted to be a part of. </h>
           </div>
           {textRow3(
             <div>
@@ -74,7 +74,7 @@ function header() {
           )} 
           <div class="image-text-wrapper">
           {imageRow3(jennifer)}
-         <b> Why  FirstByte?</b>I first joined because I wanted to help pioneer the non computer science ventures of our curriculum team. FirstByte's mission is to provide access to STEM resources, and I wanted to play a role in diversifying the array of lessons available to our students.</div>
+          <p> <b>Why FirstByte?</b></p><h>I first joined because I wanted to help pioneer the non computer science ventures of our curriculum team. FirstByte's mission is to provide access to STEM resources, and I wanted to play a role in diversifying the array of lessons available to our students.</h></div>
           {textRow3(
             <div>
             Jennifer Esfahany <br></br>
@@ -83,7 +83,7 @@ function header() {
           )} 
           <div class="image-text-wrapper">
           {imageRow3(darren)}
-          <b>Why FirstByte?</b>I had quite a bit of free time during my co-op and was wanted to do something productive. That's when a friend introduced me to this club.
+          <p> <b>Why FirstByte?</b></p><h>I had quite a bit of free time during my co-op and was wanted to do something productive. That's when a friend introduced me to this club.</h>
           </div>
            {textRow3(
             <div>
@@ -93,7 +93,7 @@ function header() {
           )} 
           <div class="image-text-wrapper">
           {imageRow3(landyn)}
-          <b>Why FirstByte?</b> I joined FirstByte my sophomore year to get more involved with community service in the Boston area while also furthering my love for STEM. Everyone should have equal access to the resources we provide, so the club's purpose really stood out to me. I also loved how welcoming and inclusive the club members were, and that there was no heavy application process that would deter anyone from wanting to contribute to the club's amazing mission. </div>
+          <p> <b>Why FirstByte?</b></p><h> I joined FirstByte my sophomore year to get more involved with community service in the Boston area while also furthering my love for STEM. Everyone should have equal access to the resources we provide, so the club's purpose really stood out to me. I also loved how welcoming and inclusive the club members were, and that there was no heavy application process that would deter anyone from wanting to contribute to the club's amazing mission. </h></div>
           {textRow3(
            <div>
            Landyn Sparacino<br></br>
@@ -102,7 +102,7 @@ function header() {
            )}
            <div class="image-text-wrapper">
           {imageRow3(win)}
-          <b>Why FirstByte?</b> I wanted to find a way to volunteer while still focusing on cs. I always loved teaching and helping people so FirstByte was a perfect fit for me! Caleb let me know about the club and I’ve been in it since!
+          <p> <b>Why FirstByte?</b></p><h> I wanted to find a way to volunteer while still focusing on cs. I always loved teaching and helping people so FirstByte was a perfect fit for me! Caleb let me know about the club and I’ve been in it since!</h>
           </div> {textRow3(
            <div>
             Win Tongtawee <br></br>
@@ -111,7 +111,7 @@ function header() {
           )}
           <div class="image-text-wrapper">
           {imageRow3(srikar)}
-         <b> Why FirstByte?</b>I've always loved teaching and working with kids, and FirstByte had the perfect intersection of Computer Science, education, and impact that I wanted </div>
+          <p> <b>Why FirstByte?</b></p><h>I've always loved teaching and working with kids, and FirstByte had the perfect intersection of Computer Science, education, and impact that I wanted.</h></div>
           {textRow3(
            <div>
             Srikar Ananthoju<br></br>
