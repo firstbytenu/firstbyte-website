bc545128c30b4d97f66623dd058edd764416af3c
sstej88 <sunkara.sai@northeastern.edu>
2024-04-11 00:18:30 -0400

    Upgraded Vite React in TypeScript

--- firstbyte/src/app/slice-simulator/page.tsx
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-import { SliceZone } from "@prismicio/react";
-
-import { components } from "@/slices";
-
-export default function SliceSimulatorPage({ searchParams }: any) {
-  return <SliceZone components={components} />;
-}
---
+++ firstbyte/src/main.tsx
@@ -0,0 +1,11 @@
+/** @format */
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const rootComponent = ReactDOM.createRoot(document.getElementById("root")!);
+rootComponent.render(
+  <React.StrictMode>
+    <h1>Hello</h1>
+  </React.StrictMode>
+);
