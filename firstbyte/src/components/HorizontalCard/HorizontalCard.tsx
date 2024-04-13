02c370ab103c1c7754d2b907bf86827efdad698c
sstej88 <sunkara.sai@northeastern.edu>
2024-04-13 00:49:55 -0400

    Removed unwanted cursor pointers

--- firstbyte/src/components/HorizontalCard/HorizontalCard.tsx
+++ firstbyte/src/components/HorizontalCard/HorizontalCard.tsx
@@ -14,7 +14,7 @@ const HorizontalCard: React.FC<HorizontalCardProps> = ({ image, text }) => {
       <motion.div
         whileHover={{ scale: 1.03 }}
         whileFocus={{ scale: 1.03 }}
-        className="mx-5 w-11/12 h-96 bg-white hover:bg-gray-100 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex items-center justify-center cursor-pointer">
+        className="mx-5 w-11/12 h-96 bg-white hover:bg-gray-100 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex items-center justify-center">
         <div className="flex flex-col items-center justify-center w-full">
           <img
             className="rounded-t-lg object-contain h-64 w-full py-5"
