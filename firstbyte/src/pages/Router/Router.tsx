ceebba9726c57a6c22f480e760e7f1958a000deb
sstej88 <sunkara.sai@northeastern.edu>
2024-04-16 17:29:16 -0400

    Implemented lazy loading optimization and loading screen added

--- firstbyte/src/pages/Router/Router.tsx
+++ firstbyte/src/pages/Router/Router.tsx
@@ -1,13 +1,16 @@
 /** @format */
 
+import React, { Suspense } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "../../styles/css/Main/main.css";
-import Home from "../Home/Home";
 import Navbar from "../../slices/Navbar/Navbar";
-import Team from "../Team/Team";
 import useIsAnimateNavbar from "../../hooks/useIsAnimateNavbar";
-import Join from "../Join/Join";
-import Curricula from "../Curricula/Curricula";
+import Loader from "../../components/Loader/Loader";
+
+const Home = React.lazy(() => import("../Home/Home"));
+const Team = React.lazy(() => import("../Team/Team"));
+const Curricula = React.lazy(() => import("../Curricula/Curricula"));
+const Join = React.lazy(() => import("../Join/Join"));
 
 const Router = () => {
   const [isAnimateNavbar, toggleIsAnimateNavbar] = useIsAnimateNavbar();
@@ -19,31 +22,44 @@ const Router = () => {
           <Route
             path="/"
             element={
-              <Home
-                isAnimateNavbar={isAnimateNavbar}
-                toggleIsAnimateNavbar={toggleIsAnimateNavbar}
-              />
+              <Suspense fallback={<Loader text={"Home Page"} />}>
+                <Home
+                  isAnimateNavbar={isAnimateNavbar}
+                  toggleIsAnimateNavbar={toggleIsAnimateNavbar}
+                />
+              </Suspense>
             }
           />
           <Route
             path="/team"
             element={
-              <Team
-                isAnimateNavbar={isAnimateNavbar}
-                toggleIsAnimateNavbar={toggleIsAnimateNavbar}
-              />
+              <Suspense fallback={<Loader text={"Team Page"} />}>
+                <Team
+                  isAnimateNavbar={isAnimateNavbar}
+                  toggleIsAnimateNavbar={toggleIsAnimateNavbar}
+                />
+              </Suspense>
             }
           />
           <Route
             path="/curricula"
             element={
-              <Curricula
-                isAnimateNavbar={isAnimateNavbar}
-                toggleIsAnimateNavbar={toggleIsAnimateNavbar}
-              />
+              <Suspense fallback={<Loader text={"Curricula Page"} />}>
+                <Curricula
+                  isAnimateNavbar={isAnimateNavbar}
+                  toggleIsAnimateNavbar={toggleIsAnimateNavbar}
+                />
+              </Suspense>
             }
           />
-          <Route path="/contact" element={<Join />} />
+          <Route
+            path="/contact"
+            element={
+              <Suspense fallback={<Loader text={"Contact Page"} />}>
+                <Join />
+              </Suspense>
+            }
+          />
           <Route path="*" element={<Navigate replace to="/" />} />
         </Routes>
       </BrowserRouter>
