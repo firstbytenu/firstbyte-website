c89d58541cc9792f2b5cd1895998a385c3ea7440
Anna Higgins <anna.p.higgins@gmail.com>
2024-08-30 22:41:32 +0100

    Updates

--- assets/Hero-3gu1WLI1.js
+++
@@ -1,1 +0,0 @@
-import{j as e,R as r}from"./index-THsr4SMP.js";import{C as o,H as x}from"./HeadingRichText-D1lqX4kd.js";const c=({children:t,headingText:n,headingIlluminateText:s,backText:a})=>e.jsx(e.Fragment,{children:e.jsx(o,{children:e.jsxs("span",{children:[e.jsx(x,{text:n,illuminateText:s,backText:a}),r.Children.map(t,i=>r.cloneElement(i,{}))]})})});export{c as H};
