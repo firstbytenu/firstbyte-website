8603c3a33e4a594ee5471d5663e60aea79fa61b2
Anna Higgins <anna.p.higgins@gmail.com>
2024-08-30 22:22:59 +0100

    Updates

--- assets/Hero-CYXsCQ1_.js
+++
@@ -1,1 +0,0 @@
-import{j as e,R as r}from"./index-TV9hYZat.js";import{C as o,H as x}from"./HeadingRichText-Cgtw5Gws.js";const c=({children:t,headingText:n,headingIlluminateText:s,backText:a})=>e.jsx(e.Fragment,{children:e.jsx(o,{children:e.jsxs("span",{children:[e.jsx(x,{text:n,illuminateText:s,backText:a}),r.Children.map(t,i=>r.cloneElement(i,{}))]})})});export{c as H};
---
+++ assets/Hero-l1XKRis9.js
@@ -0,0 +1,1 @@
+import{j as e,R as r}from"./index-BnVDqFLh.js";import{C as o,H as x}from"./HeadingRichText-Di026K8v.js";const c=({children:t,headingText:n,headingIlluminateText:s,backText:a})=>e.jsx(e.Fragment,{children:e.jsx(o,{children:e.jsxs("span",{children:[e.jsx(x,{text:n,illuminateText:s,backText:a}),r.Children.map(t,i=>r.cloneElement(i,{}))]})})});export{c as H};
